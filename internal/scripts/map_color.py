41111f074566f0829cbb50e6a090ecef1d19b4ea
Giorgos Kappes <geokapp@gmail.com>
2016-12-30 22:53:00 +0200

    fix for zero hue colors

--- internal/scripts/map_color.py
+++ internal/scripts/map_color.py
@@ -28,10 +28,15 @@ def main():
         hlscolor = colorsys.rgb_to_hls(rgbcolor[0], rgbcolor[1], rgbcolor[2])
         hlsbase = colorsys.rgb_to_hls(rgbbase[0], rgbbase[1], rgbbase[2])
 
-        # Get the rgb value of the result color. Here we use the hue value
-        # of the user color and we keep the lightness and saturation of the
-        # base color
-        rgb = colorsys.hls_to_rgb(hlscolor[0], hlsbase[1], hlsbase[2])
+        # Get the rgb value of the result color. If the hue value of the user
+        # color is not zero, then we use the hue value of the user color and
+        # we keep the lightness and saturation of the base color. Otherwise,
+        # we use the lightness of the base color and the saturation of the
+        # user color.
+        if hlscolor[0] > 0:
+            rgb = colorsys.hls_to_rgb(hlscolor[0], hlsbase[1], hlsbase[2])
+        else:
+            rgb = colorsys.hls_to_rgb(hlscolor[0], hlsbase[1], hlscolor[2])
 
         # Convert the result to hex format
         rgbbase[0] = rgb[0] * 255
